e2e2fa8e2922ecba205e28272ce48194af8e8689
Kyle Prager <kyleprager@gmail.com>
2013-02-28 10:56:49 -0800

    Completely re-did my HashTbl.  It was all wrong.

--- src/org/kprager/datastructures/hashtable/HashTbl.java
+++ src/org/kprager/datastructures/hashtable/HashTbl.java
@@ -16,7 +16,7 @@ public class HashTbl<K, V> {
     
     public static void main(String[] args) {
         HashTbl<Integer, Integer> ht = new HashTbl<>();
-        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,100,101,102,103,104,105,106};
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,100,101,102,103,104,105,106,112,134,156};
         for (int x : arr) {
             ht.add(x,x);
             int tmp = ht.get(x);
@@ -31,26 +31,73 @@ public class HashTbl<K, V> {
     
     
     private int size = 11;
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getMaxBucketSize() {
+        return maxBucketSize;
+    }
+
+    public void setMaxBucketSize(int maxBucketSize) {
+        this.maxBucketSize = maxBucketSize;
+    }
+
+    
+    private int maxBucketSize = 3;
     private List<List<HashEntry<K,V>>> list;
     
     public HashTbl() {
-        list = new ArrayList<>(size);
-        for (int i = 0; i < size; i++) {
-            list.add(new LinkedList<HashEntry<K,V>>());
-        }
+        initList();
     }
     public HashTbl(int size) {
         if (size > this.size) {
             this.size = size;
         }
+        initList();
+    }
+    
+    /**
+     * Completely clear our list if it has anything in it, and make a new one with current size
+     */
+    private void initList() {
+        if (list != null) {
+            list.clear();
+        }
         list = new ArrayList<>(size);
+        for(int i = 0; i < size; i++) {
+            list.add(new LinkedList<HashEntry<K,V>>());
+        }
     }
     
+    /**
+     * Add an entry into the HashTbl
+     * @param key
+     * @param value 
+     */
     public void add(K key, V value) {
-        int hash = key.hashCode() % size;
-        list.get(hash).add(new HashEntry<K,V>(key, value));
+        add(new HashEntry<>(key, value));
     }
     
+    /**
+     * used when rehashing
+     * @param entry 
+     */
+    private void add(HashEntry<K,V> entry) {
+        int hash = entry.key.hashCode() % size;
+        if (list.get(hash).size() >= maxBucketSize) {
+            rehash();
+            hash = entry.key.hashCode() % size;
+        }
+        list.get(hash).add(entry);
+    }
+    
+    /**
+     * Get a value you hashed given a key
+     * @param key used to retrieve your value
+     * @return returns value if it's found, otherwise returns null
+     */
     public V get(K key) {
         int hash = key.hashCode() % size;
         List<HashEntry<K,V>> bucket = list.get(hash);
@@ -64,6 +111,24 @@ public class HashTbl<K, V> {
         return null;
     }
     
+    private void rehash() {
+        // save all HashEntry objects and clear our buckets and main list
+        List<HashEntry<K,V>> entries = new LinkedList<>();
+        for (List<HashEntry<K,V>> bucket : list) {
+            for(HashEntry<K,V> entry : bucket) {
+                entries.add(entry);
+            }
+            bucket.clear();
+        }
+
+        // double the size of our array and rehash our entries back in
+        size = size*2;
+        initList();
+        for (HashEntry<K,V> entry : entries) {
+            add(entry);
+        }
+    }
+    
     public void remove(K key) {
         boolean retval = false;
         int hash = key.hashCode() % size;
@@ -74,6 +139,9 @@ public class HashTbl<K, V> {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        sb.append("Size: ");
+        sb.append(size);
+        sb.append("\n");
         for (List<HashEntry<K,V>> bucket : list) {
             sb.append(bucket);
             sb.append("\n");
